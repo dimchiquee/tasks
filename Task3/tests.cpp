37631089b4c0554d0e218edc6ff145645b06c9b9
dimchique <dmitrinolimov@gmail.com>
2023-01-17 20:29:56 +1000

    added new tests

--- Task3/tests.cpp
+++ Task3/tests.cpp
@@ -6,4 +6,6 @@ TEST(ReadLineTest, ReadLine)
     EXPECT_EQ(decodeFromBase64("cmFuZG9t"),"A");
     EXPECT_EQ(decodeFromBase64("cGl2a28="),"pivko")
     EXPECT_EQ(encodeToBase64("testing"),"dGVzdGluZw==");
+    EXPECT_EQ(encodeToBase64(""),"");
+    EXPECT_EQ(decodeFromBase64(""),"")
 }

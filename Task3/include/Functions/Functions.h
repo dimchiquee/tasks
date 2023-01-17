f56f246155a57b25f10b26572b171bc083bf1399
dimchique <115143667+dimchique@users.noreply.github.com>
2023-01-17 12:20:52 +1000

    Create Functions.h

---
+++ Task3/include/Functions/Functions.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+std::string toBin8(std::string a);
+
+std::string toBin6(int a)
+
+int binToDec(std::string s);
+
+std::string encodeToBase64(std::string s);
+
+std::string decodeFromBase64(std::string s);
+

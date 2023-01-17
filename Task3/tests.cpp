46eb50cc9745d706ac103cc857c7c44d3c81f2b9
dimchique <115143667+dimchique@users.noreply.github.com>
2023-01-17 12:18:51 +1000

    Create tests.cpp

---
+++ Task3/tests.cpp
@@ -0,0 +1,9 @@
+#include "Functions/Functions.h"
+#include "gtest/gtest.h"
+TEST(ReadLineTest, ReadLine)
+{
+    EXPECT_EQ(encodeToBase64("A"),"QQ==");
+    EXPECT_EQ(decodeFromBase64("cmFuZG9t"),"A");
+    EXPECT_EQ(decodeFromBase64("cGl2a28="),"pivko")
+    EXPECT_EQ(encodeToBase64("testing"),"dGVzdGluZw==");
+}
